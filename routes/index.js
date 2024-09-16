1304366508d13b7393568e24950dc526cda9b06d
Samuel Des Cormiers <sdcormiers@hotmail.com>
2024-09-16 11:36:41 -0400

    partie 2 terminée

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'La pizza de la MAMA!' });
 });
-
+router.post('/', function(req, res, next) {
+  console.table(req.body);
+  res.render('index', { title: 'La pizza de la MAMA!' });
+});
 module.exports = router;
